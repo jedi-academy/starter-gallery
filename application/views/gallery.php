7d8b0bb2ea0e607793e518b4922d675d6e10635e
Jeremy Yang <yy25036542@gmail.com>
2016-09-22 14:53:28 -0700

    Static to Trivial Website Conversion tutorial completed Signed-off-by:Jeremy Yang <yy25036542@gmail.com>

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -14,9 +14,9 @@
                 <span class="myhead">Simple Image Gallery</span>
                 <span class="mynav">
                     <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="about.html">About</a></li>
+                        <li><a href="/welcome">Home</a></li>
+                        <li><a href="/">Gallery</a></li>
+                        <li><a href="/about">About</a></li>
                     </ul>
                 </span>
             </div>
