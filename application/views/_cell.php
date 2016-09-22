f45b53c6035a98736e40829a6e3d2bd15570a148
Manjot Khalsa <manjotkhalsa.96@gmail.com>
2016-09-22 16:57:05 -0700

    Fixed lightbox

--- application/views/_cell.php
+++ application/views/_cell.php
@@ -1,4 +1,4 @@
-<a href="./data/{filename}}"
+<a href="./data/{filename}"
     data-lightbox="gallery"
     data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
     <img src="./data/thumb/{filename}"/>
--- changelog.md
+++ changelog.md
@@ -7,3 +7,4 @@ Manjot Khalsa
 2.0 Implemented CodeIgniter Framework
 3.0 Updated Master Template along with our Views & Controllers to complement it
 4.0 Added database support and model
+4.1 Fixed lightbox
