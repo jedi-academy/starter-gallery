75c5562ad8c30d219150baa77f957f8222fcac34
lizewu <lizewu@lizedembp>
2016-09-22 20:47:47 -0700

    Titual2

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -14,10 +14,10 @@
 		<span class="myhead">Simple Image Gallery</span>
                 <span class="mynav">
                     <ul>
-						<li><a href="/">Home</a></li>
-						<li><a href="/gallery">Gallery</a></li>
-						<li><a href="/about">About</a></li>
-					</ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/gallery">Gallery</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
                 </span>
 	</div>
 	<div class="alone"></div>
