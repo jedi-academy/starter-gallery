345338bb9807c8d86adfc303808496615cdb85de
Domyouji <tommytrann@hotmail.com>
2017-01-26 18:33:38 -0800

    Fixed
    updated gallery to fix the problem

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
 
-    </head>
-    <body>
-            <div id="content">
                 <table cols="3" class="gallery">
                     <tr>
                         <td class="oneimage">
@@ -75,6 +69,4 @@
                     </tr>
                 </table>
                 <h1>The image gallery, complete :)</h1>
-        </div>
-    </body>
-</html>
+
