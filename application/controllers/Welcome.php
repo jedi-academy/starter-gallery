398ad5208b8bec72697ab201a2f9f192b14756f4
Michael Goll <mike_g97@hotmail.com>
2017-01-26 19:57:10 -0800

    Converted basic CodeIgniter application to "good" CodeIgniter application
    Signed-off-by: Michael Goll <mike_g97@hotmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,33 +4,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application
 {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	//Index Page for this controller.
 	public function index()
 	{
-		//$this->load->view('welcome');
+
+		//get newest images from model
+		$pix = $this->images->newest();
+
+		//build array of formatted cells
+		foreach($pix as $picture) 
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+
+		$this->load->library('table');
+
+		//prime table class
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+
+		$this->table->set_template($parms);
+
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+
+
 		$this->data['pagebody'] = 'Welcome';
 		$this->render();
-	}
-
-	public function about()
-	{
-		$this->load->view('about');
-	}
-
-	public function gallery()
-	{
-		$this->load->view('gallery');
 	}
 }
--- changelog.md
+++ changelog.md
@@ -8,3 +8,5 @@ Date | Description | Name (email)
 2017/01/26 | Converted static HTML website to partial CodeIgniter web application. | Michael Goll (mike_g97@hotmail.com)
 
 2017/01/26 | Added template html that grabs the content form the appropiate pages | Maitiu Morton (maitiumorton@gmail.com)
+
+2017/01/26 | Converted basic CodeIgniter web application to "good" CodeIgniter web application. | Michael Goll (mike_g97@hotmail.com)
