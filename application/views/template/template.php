0923e84c9d8f53e742dcfc028c259e522c29e9bc
Yuheng Song <forever.love.hugh@gmail.com>
2017-10-07 11:19:46 -0700

    change the css and boostrap formate, and try to test weebhook
    Signed-off-by: Yuheng Song <forever.love.hugh@gmail.com>

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -1,12 +1,14 @@
 <html>
     <head>
         <title> Raven </title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://bootswatch.com/slate/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
     <body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
                 <a class="navbar-brand" href="/">Raven</a>
@@ -20,8 +22,11 @@
 
          {content}
     </div>
-    <div id="footer" class="span12">
+    <div id="footer" class="panel panel-default">
+
+        <div class="panel-body">
         <a>Copyright &copy; 2017,  Me</a>.
+        </div> 
     </div>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
