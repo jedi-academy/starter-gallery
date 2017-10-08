5938df7c331a84fd2d16eb4dc72cc65a19de84b4
dra-one <hsharma15@my.bcit.ca>
2017-10-08 15:08:33 -0700

    Updating the fleet
    Signed-off-by: dra-one <hsharma15@my.bcit.ca>

--- application/views/flights.php
+++ application/views/flights.php
@@ -1,5 +1,4 @@
 <h1>{title}</h1>
-<a class="btn btn-primary" href="/info/flights" target="_blank"> Show JSON </a>
 <table border="1" cellpadding="2" cellspacing="1" class="table">
     <tr>
         <th>Flight ID</th>
@@ -25,5 +24,6 @@
     </tr>
     {/flight_table}
 </table>
+<a class="btn btn-primary" href="/info/flights" target="_blank"> Show JSON </a>
 
 
