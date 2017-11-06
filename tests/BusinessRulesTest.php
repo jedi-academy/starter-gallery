bdd9f883c3fe9765be541151897c92a141a4400c
ryanhhtan <ryanhhtan@gmail.com>
2017-11-05 22:59:53 -0800

    added fly interval limit test
    Signed-off-by: ryanhhtan <ryanhhtan@gmail.com>

--- tests/BusinessRulesTest.php
+++ tests/BusinessRulesTest.php
@@ -5,6 +5,7 @@ class BusinessRulesTest extends TestCase
 {
     private $CI;
     const DAY = "2017-01-01";
+    const SECONDS_IN_MINUTE = 60;
 
     public function setUp()
     {
@@ -33,7 +34,8 @@ class BusinessRulesTest extends TestCase
     {
         $timeLimit = strtotime(self::DAY . $departureTimeLimit); 
         $flights = $this->CI->flights->all();
-        foreach ($flights as $flight) {
+        foreach ($flights as $flight) 
+        {
             $departureTime = strtotime(self::DAY . $flight['departure_time']);
             $this->assertEquals($expected, $departureTime >= $timeLimit);
         }
@@ -46,10 +48,30 @@ class BusinessRulesTest extends TestCase
     {
         $timeLimit = strtotime(self::DAY . $arrivalTimeLimit); 
         $flights = $this->CI->flights->all();
-        foreach ($flights as $flight) {
+        foreach ($flights as $flight) 
+        {
             $arrivalTime = strtotime(self::DAY . $flight['arrival_time']);
             $this->assertEquals($expected, $arrivalTime <= $timeLimit);
         }
+    }
+
+    /**
+     * @dataProvider flyIntervalLimitProvider
+     */
+    public function testFlyIntervalLimit($intervalLimit, $expected)
+    {
+        $flights = $this->CI->flights->all();
+        usort($flights, "cmp_fleet");
+        for ($i = 1; $i < sizeof($flights); ++$i)
+        {
+            if ($flights[$i]['fleet_id'] == $flights[$i - 1]['fleet_id'])
+            {
+               $departureTime = strtotime(self::DAY . $flights[$i]['departure_time']);
+               $previousArrivalTime = strtotime(self::DAY . $flights[$i - 1]['arrival_time']);
+               $interval = $departureTime - $previousArrivalTime;
+               $this->assertEquals($expected, $interval >= $intervalLimit * self::SECONDS_IN_MINUTE);
+            }
+        } 
     }
     
     /**
@@ -57,7 +79,7 @@ class BusinessRulesTest extends TestCase
      */
     public function budgetLimitProvider()
     {
-        $data = json_decode(file_get_contents("data/business_rule_budget_limit.json"),true);
+        $data = json_decode(file_get_contents("data/business_rules/budget_limit.json"),true);
         $testData = array();
         $testData[$data['case']] = array_slice($data, 1);
         return $testData;
@@ -68,7 +90,7 @@ class BusinessRulesTest extends TestCase
      */
     public function departureTimeLimitProvider()
     {
-        $data = json_decode(file_get_contents("data/business_rule_departure_time_limit.json"),true);
+        $data = json_decode(file_get_contents("data/business_rules/departure_time_limit.json"),true);
         $testData = array();
         $testData[$data['case']] = array_slice($data, 1);
         return $testData;
@@ -79,9 +101,32 @@ class BusinessRulesTest extends TestCase
      */
     public function arrivalTimeLimitProvider()
     {
-        $data = json_decode(file_get_contents("data/business_rule_arrival_time_limit.json"),true);
+        $data = json_decode(file_get_contents("data/business_rules/arrival_time_limit.json"),true);
         $testData = array();
         $testData[$data['case']] = array_slice($data, 1);
         return $testData;
     }
+    
+    /**
+     * Data provider for fly interval limit 
+     */
+    public function flyIntervalLimitProvider()
+    {
+        $data = json_decode(file_get_contents("data/business_rules/fly_interval_limit.json"),true);
+        $testData = array();
+        $testData[$data['case']] = array_slice($data, 1);
+        return $testData;
+    }
+}
+
+/* A function for usort the flights */
+function cmp_fleet($a, $b)
+{
+    $cmpFleetId = strcmp($a['fleet_id'], $b['fleet_id']);
+
+    if ($cmpFleetId == 0)
+    {
+        return strcmp($a['departure_time'], $b['departure_time']);
+    } 
+    return $cmpFleetId;
 }
