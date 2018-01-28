2616e07f38e4988dd73d0edec3daa5fdf27d4a37
aavram <aavram27@gmail.com>
2018-01-28 12:45:06 -0800

    Completed third tutorial

--- application/views/template.php
+++ application/views/template.php
@@ -15,7 +15,7 @@
                 <span class="mynav">
                     <ul>
                         <li><a href="/">Home</a></li>
-                        <li><a href="/gallery">Gallery</a></li>
+                        <li><a href="/index.php/gallery">Gallery</a></li>
                         <li><a href="/about">About</a></li>
                     </ul>
                 </span>
